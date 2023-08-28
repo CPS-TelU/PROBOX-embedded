5dc726a6ec569e585ce6a922ae3f676742fdc7af
Hazman <hazman0104@gmail.com>
2023-08-28 11:18:39 +0700

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <SPI.h>
 #include <MFRC522.h>
 #include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <ESP8266HTTPClient.h>
 
 const int lock = D1;
 const int buzzer = 9;
@@ -10,17 +12,42 @@ const int button = D0;
 #define ECHO_PIN D8
 #define ACCESS_DELAY 2000
 #define DENIED_DELAY 1000
-#define RST_PIN D3   
+#define RST_PIN D3 
 #define SS_PIN D4
-#define SSID "iPhone"
-#define PASSWORD "yakaligaskuy"
-MFRC522 mfrc522(SS_PIN, RST_PIN); // Instance of the class
+MFRC522 mfrc522(SS_PIN, RST_PIN);
 
-//bool isSolenoidActive = false; // Track solenoid status
+int val = 0;
+int duration = 0;
+int distance = 0;
+
+const char* gktep = "";
 bool isFirstTap = true;        // Track if it's the first RFID tap
 bool refresh = false;
+const char* tap = "KUNCI"; // Instance of the class
 
+// Replace with your network credentials
+const char* ssid     = "hoka";
+const char* password = "1408Hoka";
+
+// supabase credentials
+String API_URL = "https://pbzaiztldlpympnwfpuz.supabase.co";
+String API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBiemFpenRsZGxweW1wbndmcHV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTI3NjA1MTMsImV4cCI6MjAwODMzNjUxM30.tHDSYLyJkULk52TB8hM9rc6xYCse2xfu39RlgQjCIK0";
+String TableName = "sensor";
+const int httpsPort = 443;
+
+// Sending interval of the packets in seconds
+// int sendinginterval = 1200; // 20 minutes
+// //int sendinginterval = 120; // 2 minutes
+
+HTTPClient https;
+WiFiClientSecure client;
 void setup() {
+  // builtIn led is used to indicate when a message is being sent
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH); // the builtin LED is wired backwards HIGH turns it off
+  // HTTPS is used without checking credentials 
+  client.setInsecure();
+  // Connect to the WIFI 
   Serial.begin(9600);
   pinMode(lock, OUTPUT);
   pinMode(TRIG_PIN, OUTPUT);
@@ -34,15 +61,28 @@ void setup() {
   isFirstTap = true;
   SPI.begin();
   mfrc522.PCD_Init();
-  WiFi.begin(SSID, PASSWORD);
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.println("Connecting to WiFi...");
+    Serial.print(".");
   }
-  Serial.println("SUCCESS!!!");
+  
+  // Print local IP address
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
-
+String uidString = "";
+String status = "";
 void loop() {
+
+   // If connected to the internet turn the Builtin led On and attempt to send a message to the database 
+  if (WiFi.status() == WL_CONNECTED) {
+    digitalWrite(LED_BUILTIN, LOW); // LOW turns ON
+
   digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(2);
   digitalWrite(TRIG_PIN, HIGH);
@@ -60,10 +100,12 @@ void loop() {
   if (distance_cm < 30) {
     digitalWrite(LED_R, HIGH);
     Serial.println("Ada");
+    status = "ADA BARANG";
   }
   if (distance_cm > 30) {
     digitalWrite(LED_R, LOW);
     Serial.println("Kosong");
+    status = "TIDAK ADA BARANG";
   }
 
   if (digitalRead(button) == LOW) {
@@ -77,8 +119,8 @@ void loop() {
       ESP.restart();
       refresh = false;
     }
-
-  if (!mfrc522.PICC_IsNewCardPresent()) {
+    if (!mfrc522.PICC_IsNewCardPresent()) {
+    // sendBoxData(); 
     return;
   }
   if (!mfrc522.PICC_ReadCardSerial()) {
@@ -88,35 +130,65 @@ void loop() {
   Serial.print("UID tag :");
   String content = "";
   for (byte i = 0; i < mfrc522.uid.size; i++) {
-    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? "0" : "");
     Serial.print(mfrc522.uid.uidByte[i], HEX);
-    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? "0" : ""));
     content.concat(String(mfrc522.uid.uidByte[i], HEX));
   }
   Serial.println();
   content.toUpperCase();
-  
-  else if (content.substring(1) == "B4 E6 81 07" || content.substring(1) == "13 1F FB 0B") {
-    Serial.println("RFID tapped");
-    Serial.println("Authorized access (orang1)");
 
-    if (isFirstTap) {
-      Serial.println("Solenoid activated");
-      digitalWrite(lock, LOW);
-      isFirstTap = false;
-    } else {
-      Serial.println("Solenoid deactivated");
-      digitalWrite(lock, HIGH);
-      isFirstTap = true;
+  uidString = content;
+
+  
+  bool authorized = false;
+    if (uidString == "551E9552" || uidString == "584E0C53" || uidString == "BAE52B3B" || uidString == "1637C942" || uidString == "8518D952" ||
+        uidString == "351C6452" || uidString == "782D9053" || uidString == "458B2552" || uidString == "48EFC453") {
+      authorized = true;
+
+      if (uidString == "551E9552") uidString = "AWP 21";
+      else if (uidString == "584E0C53") uidString = "HZN 21";
+      else if (uidString == "351C6452") uidString = "MFA 21";
+      else if (uidString == "782D9053") uidString = "GHN 21";
+      else if (uidString == "48EFC453") uidString = "ASF 21";
+      else if (uidString == "458B2552") uidString = "OKA 21";
+      else if (uidString == "BAE52B3B") uidString = "HRI 21";
+      else if (uidString == "1637C942") uidString = "RIA 21";
+      else if (uidString == "8518D952") uidString = "RHN 21";
     }
-    delay(1000); // Delay to prevent rapid toggling due to reading multiple taps
+  if (isFirstTap) {
+    Serial.println("Solenoid activated");
+    digitalWrite(lock, LOW);
+    isFirstTap = false;
+  } else {
+    Serial.println("Solenoid deactivated");
+    digitalWrite(lock, HIGH);
+    isFirstTap = true;
   }
-  else   {
-    Serial.println("Access denied");
-    digitalWrite(buzzer, HIGH);
-    delay(1000);
-    digitalWrite(buzzer, LOW);
-  }
-  return;
+
+  tap = isFirstTap ? "KUNCI" : "BUKA";
+if (authorized) {
+  // Send the post request to SUPABASE only for authorized RFID tags
+  https.begin(client, API_URL + "/rest/v1/" + TableName);
+  https.addHeader("Content-Type", "application/json");
+  https.addHeader("Prefer", "return=representation");
+  https.addHeader("apikey", API_KEY);
+  https.addHeader("Authorization", "Bearer " + API_KEY);
+  int httpCode = https.POST("{\"uid\":\"" + uidString + "\",\"status\":\"" + status + "\",\"selenoid\":\"" + tap + "\"}");
+  String payload = https.getString(); 
+  Serial.println(httpCode);   // Print HTTP return code
+  Serial.println(payload);    // Print request response payload
+  https.end();
+
+  digitalWrite(LED_BUILTIN, HIGH); // HIGH turns off
+} else {
+  Serial.println("Access denied");
+  digitalWrite(buzzer, HIGH); // Turn on the buzzer
+  delay(1000);
+  digitalWrite(buzzer, LOW); // Turn off the buzzer
 }
 
+// Delay before sending the next request
+delay(1000);
+  }
+}
